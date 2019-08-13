ba9a70c16ef25cbb94a836902cbb403a2c0613fb
Geyan718 <30426901+Geyan718@users.noreply.github.com>
2019-08-14 00:06:49 +1000

    Add files via upload
    added crossover and mutation

--- Balance_learner.py
+++ Balance_learner.py
@@ -29,8 +29,13 @@ class balance_learner:
         data = file_contents['data']
         return attributes, data
 
+<<<<<<< HEAD
+    def create_random_chromosome(length):
+        initial = np.random.normal(0,0.1,length)
+=======
     def create_random_chromosome(self):
         initial = numpy.random.normal(0,0.1,self.chromosome_length)
+>>>>>>> 11470cefb261d67377569c4a48165432c5103a6f
         return initial
 
     def get_weights_and_biases(self, chromosome):
@@ -41,6 +46,40 @@ class balance_learner:
         weights_list = [first_layer_weights, second_layer_weights]
         biases_list = [first_layer_bias, second_layer_bias]
 
+<<<<<<< HEAD
+    def mushroom_one_point_crossover(self, parents):
+        crossover_point = np.random.randint(0, self.chromosome_length)
+        parent_1 = parents[0]
+        parent_2 = parents[1]
+        offspring_1 = parent_1[:crossover_point] + parent_2[crossover_point:]
+        offspring_2 = parent_2[:crossover_point] + parent_1[crossover_point:]
+        return [offspring_1, offspring_2]
+
+    def mushroom_two_point_crossover(self, parents):
+        crossover_points = np.random.randint(0, self.chromosome_length, 2)
+        if crossover_points[0] == crossover_points[1]:
+            offspring_1 = parents[0].copy()
+            offspring_2 = parents[1].copy()
+        else:
+            parent_1 = parents[0]
+            parent_2 = parents[1]
+            high = np.amax(crossover_points)
+            low = np.amin(crossover_points)
+            offspring_1 = parent_1[:low] + parent_2[low:high] + parent_1[high:]
+            offspring_2 = parent_2[:low] + parent_1[low:high] + parent_2[high:]
+        return [offspring_1, offspring_2]
+
+    def mutation(self, chromosome):
+
+        mutation_chance = np.random.random_sample(self.chromosome_length)
+
+        mutation_list = np.where(mutation_chance < self.ga_trainer.mutation_probability)[0].tolist()
+        for index in mutation_list:
+            increment = 1 if np.random.random() < 0.5 else -1
+            chromosome[index] += increment*0.1
+        
+        return chromosome
+=======
         return weights_list, biases_list
 
     def balance_fitness(self, chromosome):
@@ -57,7 +96,9 @@ class balance_learner:
 
     def mutation(self, chromosome):
         pass  
+>>>>>>> 11470cefb261d67377569c4a48165432c5103a6f
 
+    
     def run_simulation(self, file_name, epochs, generation_size, 
                        crossover_probability, crossover_type, mutation_probability):
         self.ga_trainer.epochs_size = epochs
