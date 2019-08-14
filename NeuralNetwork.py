18282a4935d7beec42700f8092bf0bfc8bd2aa23
Lancelot Chen <lancelot_inbox@yahoo.com.au>
2019-08-14 17:11:56 +1000

    code cleanup

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -27,40 +27,7 @@ def forward(X, weight_list, biases_list):
         input_x = output
 
     return output
-'''
-def predict(cls,X):
-    output_list = self.forward(X)
-    pred_y = self.softmax(output_list[-1])
-    return pred_y
 
-def initial_weight(self):
-
-    if self.X is not None and self.y is not None:
-        x=self.X
-        y=self.y
-        input_dim = x.shape[1]
-        output_dim = y.shape[1]
-
-        number_NN = self.neural_numbers+[output_dim]
-
-        weight_list,bias_list = [],[]
-        last_neural_number = input_dim     
-
-        for cur_neural_number in number_NN:
-                
-            # The dimension of weight matrix is last neural number * current neural number
-            weights = np.random.randn(last_neural_number, cur_neural_number)
-            # The number of dimension for bias is 1 and the number of current neural
-            bias = np.zeros((1, cur_neural_number))
-
-            last_neural_number=cur_neural_number
-
-            weight_list.append(weights)
-            bias_list.append(bias)
-
-        self.weight_list=weight_list
-        self.bias_list=bias_list
-'''
 # Classical sigmoid activation functions are used in every layer in this network
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
