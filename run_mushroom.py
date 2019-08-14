20f31410fe5a79e37885dd46ebf0f001d954f1b8
Lancelot Chen <lancelot_inbox@yahoo.com.au>
2019-08-14 17:23:31 +1000

    Wrong object called, fixed

--- run_mushroom.py
+++ run_mushroom.py
@@ -1,12 +1,12 @@
 from mushroom_learner import mushroom_learner
 
 m = mushroom_learner()
-epochs = 200
+epochs = 500
 generation_size = 100
 crossover_prob = 0.9
 crossover_type = 2
 mutation_prob = 0.05
-b.run_simulation('mushroom-{}-{}-{}-{}-{}.txt'.format(epochs, generation_size, crossover_prob,
+m.run_simulation('mushroom-{}-{}-{}-{}-{}.txt'.format(epochs, generation_size, crossover_prob,
                                                      crossover_type, mutation_prob),
                  epochs, generation_size, crossover_prob, crossover_type, mutation_prob)
 
